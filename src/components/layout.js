53228bbfa989e58ac8715bdc4d1031e2df17cc5e
Patrick San Juan <psj.webdev@gmail.com>
2018-11-03 16:19:19 -0700

    Fix build erros

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+// import { StaticQuery, graphql } from 'gatsby'
 import faunadb from 'faunadb'
 
 import seed from '../faunaDB/seeds/seed';
@@ -9,9 +9,9 @@ import seed from '../faunaDB/seeds/seed';
 import Header from './header'
 import './layout.css'
 
-const client = new faunadb.Client({ secret: 'fnAC-8NuzGACAljfuLW0PrHYpkJf6lzrHNY-gWyV' });
+// const client = new faunadb.Client({ secret: 'fnAC-8NuzGACAljfuLW0PrHYpkJf6lzrHNY-gWyV' });
 
-seed(client);
+// seed(client);
 
 const Layout = ({ children }) => (
   <StaticQuery
