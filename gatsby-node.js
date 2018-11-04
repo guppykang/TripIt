d93664691403aa677f09244a0db381d705022335
Patrick San Juan <psj.webdev@gmail.com>
2018-11-04 11:09:46 -0800

    Build fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /whatwg-fetch/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
 // You can delete this file if you're not using it
